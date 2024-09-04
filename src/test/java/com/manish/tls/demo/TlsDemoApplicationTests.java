5506ea443ff763cdd123b0e047d67d4d945a6df1
PANDEY Manish <manish.pandey3@soprasteria.com>
2024-09-04 12:42:12 +0530

    first commit

---
+++ src/test/java/com/manish/tls/demo/TlsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.tls.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
